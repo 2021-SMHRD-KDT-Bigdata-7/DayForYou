a778d5602a9bc32b190112671573a6e5e27e692c
smhrd <smhrd@DESKTOP-UVOTQGD>
2021-12-09 11:01:00 +0900

    챌린지 2번째 수정

--- DayForYou/src/main/java/model/DAO.java
+++ DayForYou/src/main/java/model/DAO.java
@@ -166,5 +166,48 @@ public class DAO {
 		
 		return ch_boards;
 	}
+	
+	/**
+	 * 챌린지 게시판을 카테고리 별로 출력하는 메소드
+	 * 
+	 * @param cat
+	 * 		  사용자가 입력한 카테고리들을 나열  
+	 */
+	
+	ArrayList<challengeBoardVO> SelectChallengeBoard_cat(String cat){
+		
+		ch_boards = new ArrayList<>(); 
+		
+		connection();
+		
+		try {
+			sql = "select * from tbl_challenge where chal_cat = ?";
+			
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, cat);
+			
+			rs = psmt.executeQuery();
+			
+			while(rs.next()) {				
+				
+				cbv = new challengeBoardVO(rs.getInt(1), rs.getString(2),
+						rs.getString(3), rs.getString(4), rs.getString(5),
+						rs.getInt(6), rs.getInt(7), rs.getString(8),
+						rs.getString(9), rs.getString(10), rs.getString(11),
+						rs.getString(12), rs.getInt(13), rs.getString(14),
+						rs.getString(15));
+				
+				ch_boards.add(cbv);				
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		
+		
+		return ch_boards;
+	}
 
 }
